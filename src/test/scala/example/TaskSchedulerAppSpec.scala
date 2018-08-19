30298f8afd35a87fa516fa5f6998e0fea63dc108
Kolade <koladeadewuyi@gmail.com>
2018-08-19 23:13:49 +0100

    Add test assertions for tree execution

--- src/test/scala/example/TaskSchedulerAppSpec.scala
+++ src/test/scala/example/TaskSchedulerAppSpec.scala
@@ -4,34 +4,44 @@ import example.model._
 import org.scalatest._
 import org.scalatest.prop.TableDrivenPropertyChecks
 
-import scala.collection.parallel.ParSeq
+import scala.collection.parallel.immutable.ParVector
 
 class TaskSchedulerAppSpec extends FunSpec with Matchers with TableDrivenPropertyChecks {
 
-  val root1 = Task("root_1")
-  val root2 = Task("root_2")
+  private val root1 = Task("root_1")
+  private val root2 = Task("root_2")
 
-  val child1 = Task("child_1", Set(root1))
-  val child2 = Task("child_2", Set(root2, child1))
-  val child3 = Task("child_3", Set(root1, root2))
+  private val child1 = Task("child_1", Set(root1))
+  private val child2 = Task("child_2", Set(root2, child1))
+  private val child3 = Task("child_3", Set(root1, root2))
+
+  private val ChildExecutionMessage = "All child task(s) in group executed"
 
   val scenarios = Table(
     ("tasks", "sequencedTasks"),
     (Seq(child1), Seq(Seq(root1, child1))),
-    (Seq(root1, root2), Seq(Seq(root1, root2))),
+    (Seq(root1, root2), Seq(Seq(root1), Seq(root2))),
     (Seq(child2), Seq(Seq(root1, child1, root2, child2))),
     (Seq(root1, root2, child1, child2), Seq(Seq(root1), Seq(root2), Seq(root1, child1), Seq(root1, child1, root2, child2)))
   )
 
   forAll(scenarios) { (tasks, sequencedTasks) =>
-    ignore(s"should return $sequencedTasks as the sequenced result of $tasks") {
+    it(s"should return $sequencedTasks as the sequenced result of $tasks") {
       new TaskSchedulerApp().sequenceTaskGroups(tasks) should contain theSameElementsInOrderAs sequencedTasks
     }
   }
 
-  forAll(scenarios) { (tasks, _) =>
+  val scenarios1 = Table(
+    ("tasks", "executionMessage"),
+    (Seq(child1), ParVector(ChildExecutionMessage)),
+    (Seq(root1, root2), ParVector.empty),
+    (Seq(child2), ParVector(ChildExecutionMessage)),
+    (Seq(root1, root2, child1, child2), ParVector(ChildExecutionMessage, ChildExecutionMessage))
+  )
+
+  forAll(scenarios1) { (tasks, result) =>
     it(s"should execute $tasks in right sequence") {
-      new TaskSchedulerApp().executeTasks(tasks) shouldBe ParSeq("")
+      new TaskSchedulerApp().executeTasks(tasks) shouldBe result
     }
   }
 }
